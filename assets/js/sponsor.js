7b586f9bead7a91272be3c72bdb696f1c7d4cf6a
dedsec11 <wahyuhidjr11@gmail.com>
2021-06-19 19:52:20 +0700

    sponsor done

--- assets/js/sponsor.js
+++ assets/js/sponsor.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".mySwipper", {
     slidesPerView: 3,
     spaceBetween: 30,
     freeMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
--- index.php
+++ index.php
@@ -208,21 +208,30 @@
         <div class="container">
             <div class="row">
                 <h1>SPONSORSHIP</h1>
-                <div class="swiper-container mySwipper">
+                <div class="swiper-container mySwipper" id="swiper-container-sponsor">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide">Slide 1</div>
-                        <div class="swiper-slide">Slide 2</div>
-                        <div class="swiper-slide">Slide 3</div>
-                        <div class="swiper-slide">Slide 4</div>
-                        <div class="swiper-slide">Slide 5</div>
-                        <div class="swiper-slide">Slide 6</div>
-                        <div class="swiper-slide">Slide 7</div>
-                        <div class="swiper-slide">Slide 8</div>
-                        <div class="swiper-slide">Slide 9</div>
+                        <div class="swiper-slide" id="swiper-slide">
+                            <img src="../Freeletics/assets/images/s1.png">
+                        </div>
+                        <div class="swiper-slide" id="swiper-slide">
+                            <img src="../Freeletics/assets/images/s3.png">
+                        </div>
+                        <div class="swiper-slide" id="swiper-slide">
+                            <img src="../Freeletics/assets/images/s3.png">
+                        </div>
+                        <div class="swiper-slide" id="swiper-slide">
+                            <img src="../Freeletics/assets/images/s1.png">
+                        </div>
+                        <div class="swiper-slide" id="swiper-slide">
+                            <img src="../Freeletics/assets/images/s3.png">
+                        </div>
+                        <div class="swiper-slide" id="swiper-slide">
+                            <img src="../Freeletics/assets/images/s3.png">
+                        </div>
                     </div>
-                    <div class="swiper-button-next" id="btn-fun"></div>
-                    <div class="swiper-button-prev" id="btn-fun"></div>
-                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-next" id="btn-sponsor"></div>
+                    <div class="swiper-button-prev" id="btn-sponsor"></div>
+                    <!-- <div class="swiper-pagination" id="pagination-sponspor"></div> -->
                 </div>
             </div>
         </div>
