b3b2bbe502ec3673bf2d8301bf46ccdeda27607a
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2021-07-15 15:17:01 +0700

    Backend Done

--- resources/views/admin/author.blade.php
+++ resources/views/admin/author.blade.php
@@ -84,11 +84,8 @@
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                            alt="User Image">
-                    </div>
                     <div class="info">
+                        <p>Hello,</p>
                         @foreach ($admin as $admin)
                         <a href="#" class="d-block">{{ $admin->name }}</a>
                         @endforeach
